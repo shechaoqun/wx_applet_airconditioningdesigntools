3774e287106660bea3b84a4bac0fac0c576fda35
lovayone <lovayone@gmail.com>
2017-10-19 15:50:46 +0800

    修改行间距

---
+++ entity/air_c_number_dict copy.js
@@ -0,0 +1,302 @@
+// 匹数设计 实体
+var air_c_number_dict = [
+  {
+    room_type: "书|卧",
+    machin_type: "风管机",
+    horses_number: "小1匹",
+    room_type_id: "0",
+    machin_type_id: "0",
+    horses_number_id: "0",
+    //冷量
+    quantity_cold: "2.2KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3250"
+  }, {
+    room_type: "书|卧",
+    machin_type: "风管机",
+    horses_number: "正1匹",
+    room_type_id: "0",
+    machin_type_id: "0",
+    horses_number_id: "1",
+    quantity_cold: "2.8KW",
+    default_num: "1",
+    price: "3360"
+  }, {
+    room_type: "书|卧",
+    machin_type: "风管机",
+    horses_number: "1.5匹",
+    room_type_id: "0",
+    machin_type_id: "0",
+    horses_number_id: "2",
+    //冷量
+    quantity_cold: "3.6KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3460"
+  }, {
+    room_type: "书|卧",
+    machin_type: "风管机",
+    horses_number: "小2匹",
+    room_type_id: "0",
+    machin_type_id: "0",
+    horses_number_id: "3",
+    //冷量
+    quantity_cold: "4KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3560"
+  }, {
+    room_type: "书|卧",
+    machin_type: "风管机",
+    horses_number: "正2匹",
+    room_type_id: "0",
+    machin_type_id: "0",
+    horses_number_id: "4",
+    //冷量
+    quantity_cold: "5.6KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3780"
+  }, {
+    room_type: "书|卧",
+    machin_type: "风管机",
+    horses_number: "3匹",
+    room_type_id: "0",
+    machin_type_id: "0",
+    horses_number_id: "5",
+    //冷量
+    quantity_cold: "9.0KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "4650"
+  }, {
+    room_type: "书|卧",
+    machin_type: "单面嵌",
+    horses_number: "小1匹",
+    room_type_id: "0",
+    machin_type_id: "1",
+    horses_number_id: "0",
+    //冷量
+    quantity_cold: "2.2KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "4880"
+  }, {
+    room_type: "书|卧",
+    machin_type: "单面嵌",
+    horses_number: "正1匹",
+    room_type_id: "0",
+    machin_type_id: "1",
+    horses_number_id: "1",
+    //冷量
+    quantity_cold: "2.8KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "5070"
+  }, {
+    room_type: "书|卧",
+    machin_type: "单面嵌",
+    horses_number: "1.5匹",
+    room_type_id: "0",
+    machin_type_id: "1",
+    horses_number_id: "2",
+    quantity_cold: "3.6KW",
+    default_num: "1",
+    price: "5280"
+  }, {
+    room_type: "书|卧",
+    machin_type: "单面嵌",
+    horses_number: "小2匹",
+    room_type_id: "0",
+    machin_type_id: "1",
+    horses_number_id: "3",
+    quantity_cold: "4.5KW",
+    default_num: "1",
+    price: "5450"
+  }, {
+    room_type: "书|卧",
+    machin_type: "单面嵌",
+    horses_number: "正2匹",
+    room_type_id: "0",
+    machin_type_id: "1",
+    horses_number_id: "4",
+    //冷量
+    quantity_cold: "5.6KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "5930"
+  }, {
+    room_type: "书|卧",
+    machin_type: "单面嵌",
+    horses_number: "3匹",
+    room_type_id: "0",
+    machin_type_id: "1",
+    horses_number_id: "5",
+    //冷量
+    quantity_cold: "7.1KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "6370"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "风管机",
+    horses_number: "小1匹",
+    room_type_id: "1",
+    machin_type_id: "0",
+    horses_number_id: "0",
+    //冷量
+    quantity_cold: "2.2KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3250"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "风管机",
+    horses_number: "正1匹",
+    room_type_id: "1",
+    machin_type_id: "0",
+    horses_number_id: "1",
+    quantity_cold: "2.8KW",
+    default_num: "1",
+    price: "3360"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "风管机",
+    horses_number: "1.5匹",
+    room_type_id: "1",
+    machin_type_id: "0",
+    horses_number_id: "2",
+    //冷量
+    quantity_cold: "3.6KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3460"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "风管机",
+    horses_number: "小2匹",
+    room_type_id: "1",
+    machin_type_id: "0",
+    horses_number_id: "3",
+    //冷量
+    quantity_cold: "4KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3560"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "风管机",
+    horses_number: "正2匹",
+    room_type_id: "1",
+    machin_type_id: "0",
+    horses_number_id: "4",
+    //冷量
+    quantity_cold: "5.6KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "3780"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "风管机",
+    horses_number: "3匹",
+    room_type_id: "1",
+    machin_type_id: "0",
+    horses_number_id: "5",
+    //冷量
+    quantity_cold: "9.0KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "4650"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "单面嵌",
+    horses_number: "小1匹",
+    room_type_id: "1",
+    machin_type_id: "1",
+    horses_number_id: "0",
+    //冷量
+    quantity_cold: "2.2KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "4880"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "单面嵌",
+    horses_number: "正1匹",
+    room_type_id: "1",
+    machin_type_id: "1",
+    horses_number_id: "1",
+    //冷量
+    quantity_cold: "2.8KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "5070"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "单面嵌",
+    horses_number: "1.5匹",
+    room_type_id: "1",
+    machin_type_id: "1",
+    horses_number_id: "2",
+    quantity_cold: "3.6KW",
+    default_num: "1",
+    price: "5280"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "单面嵌",
+    horses_number: "小2匹",
+    room_type_id: "1",
+    machin_type_id: "1",
+    horses_number_id: "3",
+    quantity_cold: "4.5KW",
+    default_num: "1",
+    price: "5450"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "单面嵌",
+    horses_number: "正2匹",
+    room_type_id: "1",
+    machin_type_id: "1",
+    horses_number_id: "4",
+    //冷量
+    quantity_cold: "5.6KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "5930"
+  }, {
+    room_type: "客|餐厅",
+    machin_type: "单面嵌",
+    horses_number: "3匹",
+    room_type_id: "1",
+    machin_type_id: "1",
+    horses_number_id: "5",
+    //冷量
+    quantity_cold: "7.1KW",
+    //默认需求数量
+    default_num: "1",
+    //价格
+    price: "6370"
+  }]
+
+
+module.exports = {
+  air_c_number_dict: air_c_number_dict
+}
